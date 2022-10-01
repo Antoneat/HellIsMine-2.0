8bda295343b9744e8810450c4ebd521cb2656018
JoseCabrera9608 <Jecc9608@gmail.com>
2022-10-01 18:13:00 -0500

    Movimiento lolsito

---
+++ Assets/Scripts/JoseScripts/PlayerMovementNavMesh.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerMovementNavMesh : MonoBehaviour
+{
+    
+    const float smoothLikeButterTimeAnimation = .1f;
+    NavMeshAgent agent;
+    Animator anim;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        anim.SetFloat("speed", speedPercent, smoothLikeButterTimeAnimation, Time.deltaTime);
+    }
+
+    public void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
+
+    }
+}
