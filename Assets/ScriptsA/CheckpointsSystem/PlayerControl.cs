92689ecf20fdda6016bf130f13c499322d59a7ed
Walter Sáenz <walterantonio16@gmail.com>
2022-10-14 12:50:44 -0500

    Scarlet de sandbox a jugable, checkpoints y bugs de animaciones arregladas

---
+++ Assets/ScriptsA/CheckpointsSystem/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    CheckpointSystem checkpointSystem;
+    public Vector3 playerInitialPosition;
+
+    void Start()
+    {
+        playerInitialPosition = transform.position;
+        checkpointSystem = FindObjectOfType<CheckpointSystem>();
+    }
+
+    public void SetPosition(Vector3 p)
+    {
+        transform.position = p;
+    }
+
+    public void ResetPlayer()
+    {
+        if (checkpointSystem.CheckpointAvailable())
+        {
+            transform.position = checkpointSystem.GetCurrentCheckpointPosition();
+        }
+        else
+        {
+            transform.position = playerInitialPosition;
+        }
+    }
+}
