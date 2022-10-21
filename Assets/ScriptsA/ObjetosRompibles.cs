0cbc53512b53f1111c0e06e68ace50e831b55d1c
UGPDP <mar-kazo28@hotmail.com>
2022-10-20 20:45:01 -0500

    Implementacion de Sonidos y correcion de Script
    -Sonidos: background, arena de almas, pila de huesos, carcel, antorchas, arbustos
    -Script de destruir objetos: optimizado

--- Assets/ScriptsA/ObjetosRompibles.cs
+++ Assets/ScriptsA/ObjetosRompibles.cs
@@ -1,28 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ObjetosRompibles : MonoBehaviour
 {
     public int vida = 1;
+    [SerializeField] private float time;
+    [SerializeField] private UnityEvent OnDestroy;
+    private MeshRenderer mesh;
+    private Collider collider;
 
-    void Update()
+    private void Awake()
     {
-        Muerte();
+        mesh = GetComponent<MeshRenderer>();
+        collider = GetComponent<Collider>();
     }
 
-    private void Muerte()
+
+    private IEnumerator Muerte()
     {
-        if (vida <= 0)
-        {
-            Destroy(gameObject);
-        }
+        if (vida > 0) yield break;
+        OnDestroy.Invoke();
+        if (mesh) mesh.enabled = false;
+        if (collider) collider.enabled = false;
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.CompareTag("Guadana")) vida--;
-
         if (collider.gameObject.CompareTag("AtaqueDuro")) vida--;
+        StartCoroutine(Muerte());
     }
 }
