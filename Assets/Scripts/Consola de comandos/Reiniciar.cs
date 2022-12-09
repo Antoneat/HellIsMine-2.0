cd30a74122de7a595a82e0da604d7b09b33effa2
Walter Sáenz <walterantonio16@gmail.com>
2022-12-09 00:31:32 -0500

    xd

--- Assets/Scripts/Consola de comandos/Reiniciar.cs
+++ Assets/Scripts/Consola de comandos/Reiniciar.cs
@@ -8,8 +8,6 @@ public class Reiniciar : MonoBehaviour
 
     public GameObject scarletPrefab;
 
-    public Transform spawnScarlet;
-
     public ConsolaComandosManager consolaComandos;
 
     public int numDeEscena;
